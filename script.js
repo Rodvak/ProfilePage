a534d5e65fb1319159d91b4955ad57b1c99147a7
Alex <av.rodriguez@outlook.com>
2022-03-08 13:36:31 -0800

    Profile Page

---
+++ script.js
@@ -0,0 +1,13 @@
+var changename = document.querySelector("#changename")
+function change(){
+    changename.innerText="Alice Cabrera"
+}
+var removeuser= document.querySelector("#removeuser")
+function hide(){
+    removeuser.remove()
+}
+var removeuser1= document.querySelector("#removeuser1")
+function hide1(){
+    removeuser1.remove()
+}
+
